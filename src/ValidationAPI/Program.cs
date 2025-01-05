37a4ca40b2bd4b64a608439d2e7d4f2fba8bb5b0
Stefano <schmidtsigma@gmail.com>
2025-01-05 17:50:20 +0300

    add project template

---
+++ src/ValidationAPI/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ValidationAPI;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		builder.Services.AddOpenApi();
+
+		var app = builder.Build();
+
+		if (app.Environment.IsDevelopment())
+		{
+			app.MapOpenApi();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.Run();
+	}
+}
