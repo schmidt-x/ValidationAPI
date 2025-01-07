1cd485970b85cf1cfb2dac54985f8eb1bda8e228
Stefano <schmidtsigma@gmail.com>
2025-01-07 14:27:57 +0300

    add User entity

---
+++ src/ValidationAPI/Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ValidationAPI.Domain.Entities;
+
+public class User
+{
+	public Guid Id { get; init; }
+	public string Email { get; init; } = null!;
+	public string NormalizedEmail { get; init; } = null!;
+	public string Username { get; init; } = null!;
+	public string NormalizedUsername { get; init; } = null!;
+	public string PasswordHash { get; init; } = null!;
+	public bool IsConfirmed { get; init; }
+	public DateTimeOffset CreatedAt { get; init; }
+	public DateTimeOffset ModifiedAt { get; init; }
+}
