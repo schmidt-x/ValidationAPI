5ac3c083e5309dcca7a8d2f4611baa6e6cf40a75
Stefano <schmidtsigma@gmail.com>
2025-01-07 15:38:41 +0300

    add initial migration

---
+++ src/ValidationAPI/Data/Migrations/InitialMigration_2025010701.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+
+namespace ValidationAPI.Data.Migrations;
+
+[Migration(20250107_01)]
+public class InitialMigration_2025010701 : Migration
+{
+	public override void Up()
+	{
+		const string sql = """
+			CREATE TABLE IF NOT EXISTS users (
+				id                  UUID PRIMARY KEY,
+				email               TEXT NOT NULL,
+				normalized_email    TEXT NOT NULL UNIQUE,
+				username            TEXT NOT NULL,
+				normalized_username TEXT NOT NULL UNIQUE,
+				password_hash       TEXT NOT NULL,
+				is_confirmed        BOOLEAN,
+				created_at          TIMESTAMPTZ NOT NULL,
+				modified_at         TIMESTAMPTZ NOT NULL
+			)
+			""";
+		
+		Execute.Sql(sql);
+	}
+
+	public override void Down()
+	{
+		const string sql = "DROP TABLE IF EXISTS users";
+		Execute.Sql(sql);
+	}
+}
