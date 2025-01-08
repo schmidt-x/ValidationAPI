ce67e75eed78b0940e28cd595ac05fcf961962a7
Stefano <schmidtsigma@gmail.com>
2025-01-08 19:33:08 +0300

    move FailResponse helper functions

--- src/ValidationAPI/Extensions/ValidationExceptionExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using ValidationAPI.Common.Exceptions;
-using ValidationAPI.Responses;
-
-namespace ValidationAPI.Extensions;
-
-public static class ValidationExceptionExtensions
-{
-	public static FailResponse ToFailResponse(this ValidationException ex) => new(ex.Code, ex.Message, ex.Errors);
-}
--- src/ValidationAPI/Responses/FailResponse.cs
+++ src/ValidationAPI/Responses/FailResponse.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections.Generic;
+using ValidationAPI.Common.Exceptions;
 using ValidationAPI.Common.Models;
 
 namespace ValidationAPI.Responses;
 
-public record FailResponse(string Code, string Message, Dictionary<string, List<ErrorDetail>>? Errors);
+public record FailResponse(string Code, string Message, Dictionary<string, List<ErrorDetail>> Errors)
+{
+	public static FailResponse From(AuthException ex) => new(ex.Code, ex.Message, []);
+	public static FailResponse From(ValidationException ex) => new(ex.Code, ex.Message, ex.Errors);
+}
