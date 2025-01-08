8baba240d261a7ab43143388f02137349efbb32d
Stefano <schmidtsigma@gmail.com>
2025-01-08 19:32:26 +0300

    add AuthException

---
+++ src/ValidationAPI/Common/Exceptions/AuthException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ValidationAPI.Domain.Constants;
+
+namespace ValidationAPI.Common.Exceptions;
+
+public class AuthException : Exception
+{
+	public string Code => ErrorCodes.AUTH_FAILURE;
+	
+	public AuthException() : base("Login/password is incorrect.")
+	{ }
+}
