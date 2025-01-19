3597dff059c046df35fb9bafc62be0f050f037c3
Stefano <schmidtsigma@gmail.com>
2025-01-19 12:30:11 +0300

    add DictionaryExtensions

---
+++ src/ValidationAPI/Common/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using ValidationAPI.Common.Models;
+
+namespace ValidationAPI.Common.Extensions;
+
+public static class DictionaryExtensions
+{
+	public static void AddErrorDetail(
+		this Dictionary<string, List<ErrorDetail>> errors, string key, string code, string errorMessage)
+	{
+		if (errors.TryGetValue(key, out var details))
+		{
+			details.Add(new ErrorDetail(code, errorMessage));
+		}
+		else
+		{
+			errors.Add(key, [ new ErrorDetail(code, errorMessage) ]);
+		}
+	}
+}
